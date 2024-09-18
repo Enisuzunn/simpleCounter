354139ca2fa3be99daf05fcab4b5b9ca3332bfd1
Enisuzunn <enisbesiktasli@gmail.com>
2024-09-18 12:36:37 +0300

    first commit

--- Sayaclar/ViewController.swift
+++ Sayaclar/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var zamanLabel: UILabel!
+    var timer = Timer()
+    var kalanZaman = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        kalanZaman = 15
+        zamanLabel.text = "Zaman: \(kalanZaman)"
     }
 
-
+    @IBAction func baslatButonu(_ sender: Any) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector (timerFonksiyonu), userInfo: nil, repeats: true)
+    }
+    @objc func timerFonksiyonu(){
+        zamanLabel.text = "Zaman: \(kalanZaman)"
+        kalanZaman = kalanZaman - 1
+        
+        if kalanZaman == -2{
+            zamanLabel.text = "Süre Bitti"
+            timer.invalidate()
+            kalanZaman = 15
+        }
+    }
 }
 
